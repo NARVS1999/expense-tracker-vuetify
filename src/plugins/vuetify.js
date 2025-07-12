938db46e01b84fca042e6df57cf13e05945349ef
John Paul <johnpaulnarvasa6@gmail.com>
2025-07-12 19:33:09 +0800

    first commit

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,32 @@
+/**
+ * plugins/vuetify.js
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Composables
+import { createVuetify } from 'vuetify'
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#3f51b5',      // indigo
+          secondary: '#7986cb',    // light indigo
+        },
+      },
+    },
+  },
+})
